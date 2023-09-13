ff51426010b9b447a3cfa11e81526c4767af92fd
_ <128780634+HatataMakuna@users.noreply.github.com>
2023-09-13 17:29:24 +0800

    aws setup

---
+++ server/config.py
@@ -0,0 +1,6 @@
+customhost = "focs-website.c1efdodxxtlq.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "ugavemechills"
+customdb = "focs-website"
+custombucket = "focs-bucket"
+customregion = "us-east-1"
