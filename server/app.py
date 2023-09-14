99c802d7917af43087949c748a181b9dc0cf26d5
_ <128780634+HatataMakuna@users.noreply.github.com>
2023-09-14 12:47:57 +0800

    minor fix

--- server/app.py
+++ server/app.py
@@ -45,8 +45,8 @@ def AddEmp():
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3.Bucket(config.custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=config.custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
             if s3_location is None:
@@ -56,7 +56,7 @@ def AddEmp():
 
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
-                custombucket,
+                config.custombucket,
                 emp_image_file_name_in_s3)
 
         except Exception as e:
